cc74265010c661b2421c6bac6576510d36c48d48
BodoWT <victorien.lague@gmail.com>
2024-02-09 09:38:04 +0100

    serveur

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const MonoClient = require("mongodb").MongoClient;
 const url = "mongodb://localhost:27017";
-const dbName = "monProjetMongo";
+const dbName = "project.CRUD";
 MonoClient.connect(url, function(err, client){
     console.log("Connecté à MongoDB");
     const db = client.db(dbName);
