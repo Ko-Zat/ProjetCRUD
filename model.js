40a3214e4ce2f64b632cd01b5ee7271256fa25d6
Kstoums <kstoums@gmail.com>
2024-02-09 11:41:28 +0100

    Update userModel and add mongoose library

--- model.js
+++ model.js
@@ -1,6 +1,10 @@
-var userModel = {
-    userName: "",
-    email: "",
-    password: "",
-    birth: "",
-}
+const mongoose= require('mongoose');
+
+const userSchema = new mongoose.Schema({
+    userName: String,
+    password: String,
+    mailAddress: String,
+    birthday: String,
+})
+
+const userModel = mongoose.model("User", userSchema);
